6fbe881d6d37030ba68ed7c325e53af5e0ffb032
Jasper Wang <jasper.wj@foxmail.com>
2017-06-28 18:10:17 +0800

    learn about chapter5 The Django Admin Site.

--- books/models.py
+++ books/models.py
@@ -2,23 +2,37 @@ from django.db import models
 
 # Create your models here.
 class Publisher(models.Model):
-	name = models.CharField(max_lengh=30)
-	address = models.CharField(max_lengh=50)
-	city = model.CharField(max_lengh=60)
-	state_province = models.CharField(max_lengh=30)
-	country = models.CharField(max_lengh=50)
+	name = models.CharField(max_length=30)
+	address = models.CharField(max_length=50)
+	city = models.CharField(max_length=60)
+	state_province = models.CharField(max_length=30)
+	country = models.CharField(max_length=50)
 	website = models.URLField()
 
-class Author(models.Model):
-	first_name = models.CharField(max_lengh=30)
-	last_name = models.CharField(max_lengh=40)
-	email = models.EmailField()
+	def __str__(self):
+		return u'%s %s' % (self.name, self.city)
+	class Meta:
+		ordering = ['name']
 
+class Author(models.Model):
+	first_name = models.CharField(max_length=30)
+	last_name = models.CharField(max_length=40)
+	email = models.EmailField(blank=True, verbose_name='e-mail')
+
+	def __str__(self):
+		return u'%s %s' % (self.first_name, self.last_name)
+ 
 class Book(models.Model):
-	title = models.CharField(max_lengh=100)
-	authors = models.ManyToMany(Author)
+	title = models.CharField(max_length=100)
+	authors = models.ManyToManyField(Author)
 	publisher = models.ForeignKey(Publisher)
-	publication_date = models.DateField()
+	publication_date = models.DateField(blank=True, null=True)
+
+	class Meta:
+		ordering = ['title']
+
+	def __str__(self):
+		return self.title
 
 
 
